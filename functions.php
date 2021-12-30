4cafbdf8da9dc1197c6aec6dec6f81a53a07e7dc
Darly Agostinho <darlyagostinho@gmail.com>
2021-12-29 21:34:34 -0300

    mobile hero

--- functions.php
+++ functions.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * erdreams functions and definitions
+ * daws functions and definitions
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package erdreams
+ * @package daws
  */
 
 if ( ! defined( '_S_VERSION' ) ) {
@@ -12,7 +12,7 @@ if ( ! defined( '_S_VERSION' ) ) {
 	define( '_S_VERSION', time() );
 }
 
-if ( ! function_exists( 'erdreams_setup' ) ) :
+if ( ! function_exists( 'daws_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
@@ -20,14 +20,14 @@ if ( ! function_exists( 'erdreams_setup' ) ) :
 	 * runs before the init hook. The init hook is too late for some features, such
 	 * as indicating support for post thumbnails.
 	 */
-	function erdreams_setup() {
+	function daws_setup() {
 		/*
 		 * Make theme available for translation.
 		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on erdreams, use a find and replace
-		 * to change 'erdreams' to the name of your theme in all the template files.
+		 * If you're building a theme based on daws, use a find and replace
+		 * to change 'daws' to the name of your theme in all the template files.
 		 */
-		load_theme_textdomain( 'erdreams', get_template_directory() . '/languages' );
+		load_theme_textdomain( 'daws', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
@@ -113,7 +113,7 @@ if ( ! function_exists( 'erdreams_setup' ) ) :
 		) );
 	}
 endif;
-add_action( 'after_setup_theme', 'erdreams_setup' );
+add_action( 'after_setup_theme', 'daws_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -122,59 +122,38 @@ add_action( 'after_setup_theme', 'erdreams_setup' );
  *
  * @global int $content_width
  */
-function erdreams_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'erdreams_content_width', 600 );
+function daws_content_width() {
+	$GLOBALS['content_width'] = apply_filters( 'daws_content_width', 600 );
 }
-add_action( 'after_setup_theme', 'erdreams_content_width', 0 );
+add_action( 'after_setup_theme', 'daws_content_width', 0 );
 
 /**
  * Enqueue scripts and styles.
  */
-function erdreams_scripts() {
+function daws_scripts() {
 	/**
 	 * Fonts
 	*/
-	wp_enqueue_style( 'erdreams-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap' );
+	wp_enqueue_style( 'daws-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap' );
 
-	wp_enqueue_style( 'erdreams-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'erdreams-style', 'rtl', 'replace' );
+	wp_enqueue_style( 'daws-style', get_stylesheet_uri(), array(), _S_VERSION );
+	wp_style_add_data( 'daws-style', 'rtl', 'replace' );
 
 	/**
 	 * Scripts
 	*/
-
-	/**
-	 * Register Scripts
-	*/
-	// wp_register_script( 'splide-min', get_template_directory_uri() . '/assets/js/vendor/splide.min.js', array( 'jquery', 'bootstrap' ), _S_VERSION, true );
-	// wp_register_script( 'splide', get_template_directory_uri() . '/assets/js/splide.js', array('jquery', 'bootstrap'), _S_VERSION, true );
-	wp_register_script( 'hes-gallery', get_template_directory_uri() . '/assets/js/vendor/hes-gallery.min.js', array( 'jquery' ), _S_VERSION, true );
-
 	/**
 	 * Enqueue Scripts
 	*/
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/assets/js/vendor/bootstrap.bundle.min.js', array('jquery'), _S_VERSION, true );
 
-	wp_enqueue_script( 'erdreams-cart', get_template_directory_uri() . '/assets/js/cart.js', array('jquery'), _S_VERSION, true );
-
-	wp_enqueue_script( 'erdreams-nav', get_template_directory_uri() . '/assets/js/nav.js', array( 'jquery' ), _S_VERSION, true );
-
-	wp_enqueue_script( 'erdreams-aws-plugin', get_template_directory_uri() . '/assets/js/vendor/aws-plugin.js', array( 'jquery' ), _S_VERSION, true );
-
-	if ( is_page_template( 'template-home.php' ) ){
-		wp_enqueue_script( 'splide-min');
-		wp_enqueue_script( 'splide');
-	}
-
-	if ( is_page_template( 'template-about.php' ) ){
-		wp_enqueue_script( 'hes-gallery');
-	}
+	// wp_enqueue_script( 'daws-nav', get_template_directory_uri() . '/assets/js/nav.js', array( 'jquery' ), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
-add_action( 'wp_enqueue_scripts', 'erdreams_scripts' );
+add_action( 'wp_enqueue_scripts', 'daws_scripts' );
 
 /**
  * Utilities functions
@@ -201,7 +180,7 @@ require get_template_directory() . '/inc/template-functions.php';
  * Woocommerce custom hooks
  */
 if( class_exists( 'woocommerce' ) ){
-	require get_template_directory() . '/inc/woocommerce/erdreams-woocommerce-hooks.php';
+	require get_template_directory() . '/inc/woocommerce/daws-woocommerce-hooks.php';
 }
 
 /**
--- inc/utilities.php
+++ inc/utilities.php
@@ -3,7 +3,7 @@
  * Utilities Functions 
  */
 
- function erdreams_get_logged_user(){
+ function daws_get_logged_user(){
    $current_user = "";
    if ( is_user_logged_in() ) {
      $user = wp_get_current_user(); 
