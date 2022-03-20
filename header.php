a8619623b38315d2beefd1f50b6451f1b418bf56
Darly Agostinho <darlyagostinho@gmail.com>
2022-03-20 15:50:16 -0300

    add google tag manager tag

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The header for our theme
  *
@@ -14,16 +15,39 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="profile" href="https://gmpg.org/xfn/11">
-  <?php wp_head(); ?>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
 
-  <?php if ( true == get_theme_mod( 'sec_google_analytics_hide', true ) )  : ?>
-  <?php echo get_theme_mod( 'set_google_analytics_config' ); ?>
-  <?php endif;?>
+    <?php if (true == get_theme_mod('sec_google_analytics_hide', true)) : ?>
+    <?php echo get_theme_mod('set_google_analytics_config'); ?>
+    <?php endif; ?>
+
+    <!-- Google Tag Manager -->
+    <script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime(),
+            event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-P5CXLTF');
+    </script>
+    <!-- End Google Tag Manager -->
 
 </head>
 
 <body <?php body_class(); ?>>
-  <?php wp_body_open(); ?>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5CXLTF" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php wp_body_open(); ?>
